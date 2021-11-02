a28b393c5e61c3febf9e702e1fa4c9e2ca169c45
kishimoto <drehbleistift@gmail.com>
2021-11-02 15:27:21 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -18,27 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBWlG1WJloYOUaKr3NNdQ43ojN0-1H2vt0",
-  authDomain: "tokakuka.firebaseapp.com",
-  projectId: "tokakuka",
-  storageBucket: "tokakuka.appspot.com",
-  messagingSenderId: "358352064121",
-  appId: "1:358352064121:web:c99b39e9a055b143254ea8",
-  measurementId: "G-H1Y22DCTZ5"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
