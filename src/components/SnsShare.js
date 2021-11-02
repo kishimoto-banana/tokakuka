bde817f35d50eb6d843ec917f1e67e6a642de51e
kishimoto <drehbleistift@gmail.com>
2021-11-02 22:28:15 +0900

    Add line

--- src/components/SnsShare.js
+++ src/components/SnsShare.js
@@ -4,15 +4,17 @@ import {
   FacebookIcon,
   TwitterShareButton,
   TwitterIcon,
+  LineShareButton,
+  LineIcon,
 } from "react-share";
 
-const SnsShare = ({ shareUrls }) => {
+const SnsShare = ({ shareUrls, shareTitle }) => {
   console.log(shareUrls.facebook);
   return (
     <Grid container justifyContent="center" spacing={2}>
       {typeof shareUrls.twitter !== "undefined" && (
         <Grid item>
-          <TwitterShareButton url={shareUrls.twitter}>
+          <TwitterShareButton url={shareUrls.twitter} title={shareTitle} >
             <TwitterIcon size={32} round />
           </TwitterShareButton>
         </Grid>
@@ -20,11 +22,19 @@ const SnsShare = ({ shareUrls }) => {
 
       {typeof shareUrls.facebook !== "undefined" && (
         <Grid item>
-          <FacebookShareButton url={shareUrls.facebook}>
+          <FacebookShareButton url={shareUrls.facebook} title={shareTitle} >
             <FacebookIcon size={32} round />
           </FacebookShareButton>
         </Grid>
       )}
+
+      {typeof shareUrls.line !== "undefined" && (
+            <Grid item>
+              <LineShareButton url={shareUrls.line} title={shareTitle} >
+                <LineIcon size={32} round />
+              </LineShareButton>
+            </Grid>
+          )}
     </Grid>
   );
 };
